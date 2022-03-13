54e42ceac9ad66774515d785cb6202f303062e20
4zv4l <simon.blacks.be@gmail.com>
2022-03-13 21:44:21 +0100

    builtin words list in a separate file

---
+++ words.go
@@ -0,0 +1,22 @@
+package main
+
+
+var words = []string{
+	"apple",
+	"music",
+	"thing",
+	"child",
+	"night",
+	"world",
+	"house",
+	"water",
+	"heart",
+	"light",
+	"sound",
+	"place",
+	"right",
+	"black",
+	"white",
+	"green",
+	"happy",
+}
